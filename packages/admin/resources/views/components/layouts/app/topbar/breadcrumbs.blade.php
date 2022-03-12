39cf1c712805420c15df65f4db3aabe3b43fe92b
ndinhbang <ndinhbang111@gmail.com>
2022-03-12 19:39:05 +0700

    fix blade errors

--- packages/admin/resources/views/components/layouts/app/topbar/breadcrumbs.blade.php
+++ packages/admin/resources/views/components/layouts/app/topbar/breadcrumbs.blade.php
@@ -6,7 +6,7 @@
     <ul {{ \Illuminate\Support\Arr::toCssClasses([
         'hidden gap-4 items-center font-medium text-sm lg:flex',
         'dark:text-white' => config('filament.dark_mode'),
-    ])>
+    ]) }}>
         @foreach ($breadcrumbs as $url => $label)
             <li>
                 <a
@@ -15,7 +15,7 @@
                         'dark:text-gray-300' => (! $loop->last) && config('filament.dark_mode'),
                         'text-gray-500' => $loop->last,
                         'dark:text-gray-400' => $loop->last && config('filament.dark_mode'),
-                    ])
+                    ]) }}
                 >
                     {{ $label }}
                 </a>
@@ -25,7 +25,7 @@
                 <li {{ \Illuminate\Support\Arr::toCssClasses([
                     'h-6 border-r border-gray-300 -skew-x-12',
                     'dark:border-gray-500' => config('filament.dark_mode'),
-                ])></li>
+                ]) }}></li>
             @endif
         @endforeach
     </ul>
